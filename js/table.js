957e902dcce028bf99371ad37d306a55a0882dd0
Trup10ka <lukys3036@gmail.com>
2024-05-15 12:54:33 +0200

    05/15/2024 12:54:30

--- js/table.js
+++ js/table.js
@@ -248,6 +248,7 @@ $("#export-btn").click(function () {
 
     const thElements = document.querySelectorAll('th.sorting');
     const filteredColumns = Array.from(thElements).filter(th => th.style.width > 0);
+    console.log(filteredColumns)
 
     for (let i = 1; i < columns.length / 2; i++) {
         if (columns[i].classList[0] == "hidec")
